5bc1d1308bd3d4d23b7d1626c93601825c06ce49
IInfinity <I.INF@Outlook.com>
2019-07-25 02:01:35 +0800

    feat(main): add version command

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,35 +1,34 @@
 #!/usr/bin/env node
 import * as Commander from 'commander';
-import { readFileSync } from 'fs';
 import { dest as save, src as load } from 'gulp';
 import * as replace from 'gulp-replace';
 import { join, resolve } from 'path';
 import { RegKey, RegVar } from './reg';
+import { getVersionOfGlobal, getVersionOfLocal } from './version';
 
-const ROOT = join(__dirname, '..');
-const WORK = resolve('.');
-
-const tempFiles = {
-  cli: JSON.parse(readFileSync(join(ROOT, 'package.json')).toString()),
-  dep: JSON.parse(readFileSync(join(ROOT, 'template/package.json')).toString())
-};
-
-const VERSION = {
-  cli: tempFiles.cli.version,
-  core: tempFiles.dep.dependencies['@rester/core'],
-  typescript: tempFiles.dep.devDependencies.typescript
-};
+const GLOBAL = join(__dirname, '..');
+const LOCAL = resolve('.');
 
 Commander
-  .version(VERSION.cli)
   .command('version')
   .description('Show version of all Rester components.')
   .action(() => {
+    const global = getVersionOfGlobal();
     console.log(`
-  Rester CLI : ${VERSION.cli}
-  Rester Core: ${VERSION.core}
-  TypeScript : ${VERSION.typescript}
+  === Global Version ===
+  Rester CLI : ${global.cli}
+  Rester Core: ${global.core}
+  TypeScript : ${global.typescript}
     `);
+    try {
+      const local = getVersionOfLocal();
+      console.log(`
+  === Local Version ===
+  Rester CLI : ${local.cli}
+  Rester Core: ${local.core}
+  TypeScript : ${local.typescript}
+  `);
+    } catch (err) { }
   });
 
 Commander
@@ -37,17 +36,24 @@ Commander
   .alias('n')
   .description('Create a new rester project.')
   .action((name: string) => {
-    load(join(ROOT, 'template/**/*'))
+    load(join(GLOBAL, 'template/**/*'))
       .pipe(replace(RegVar.template, ((match: string, key: string) => {
         switch (key) {
           case RegKey.name: return name;
           default: return match;
         }
       }) as any))
-      .pipe(save(join(WORK, name)));
+      .pipe(save(join(LOCAL, name)));
     console.log(`
     Created project: ${name}
     `);
+  });
+
+Commander
+  .command('test <name>')
+  .option('-p, --part')
+  .action((name, option) => {
+    console.log(option.part);
   });
 
 Commander
