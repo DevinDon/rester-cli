1a7c0207f7ddf088e16482133e90904c8b6c6432
Devin Don <I.INF@Outlook.com>
2020-07-25 17:56:47 +0000

    feat: add commander

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,17 +1,37 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import { generateComponent } from './function';
+import { GenerateType } from './model';
 
-export const VERSION = '0.0.0';
+export const VERSION = '0.10.12';
 export const program = new Command();
 
 // describe this project
 program
   .version(VERSION)
-  .description('This is a template of command project with typescript.');
+  .description('Rester Framework Command-Line Interface.');
+
+// generate
+program
+  .command('generate <type> <name>')
+  .alias('g')
+  .description('Generate component/handler and so on.')
+  .action((type: GenerateType, name: string) => {
+    switch (type) {
+      case 'component':
+      case 'c':
+        generateComponent(name);
+        console.log(`Generated ${name} component.`);
+        break;
+      default:
+        console.warn(`Not support type ${type}`);
+        break;
+    }
+  });
 
 // tips of no such command, parse argv
 program
   .on('command:*', () => {
-    console.error(`No such command, see -h.`);
+    console.error('No such command, see -h.');
   })
   .parse(process.argv);
